4d4f5f8e1e8241fefc1ceac2753e6cf14092dabd
13291 <NickTsai@yuanta.com>
2018-08-23 15:28:23 +0800

    Add Base LiteDBService

--- DailyHome.App/Services/LiteDBService.cs
+++ DailyHome.App/Services/LiteDBService.cs
@@ -10,68 +10,50 @@ namespace DailyHome.App.Services
 {
     public class LiteDBService<T> where T:IPID
     {
-        private LiteCollection<T> LitedbCollection { get; set; }
+        public LiteCollection<T> DataList { get; set; }
         private string Table = typeof(T).ToString();
 
         public LiteDBService()
         {
             LiteDBHelper.Open(db =>
             {
-                LitedbCollection = db.GetCollection<T>(Table);
+                DataList = db.GetCollection<T>(Table);
+            });
+        }
+
+        public LiteDBService(string connString)
+        {
+            LiteDBHelper.Open(connString,db =>
+            {
+                DataList = db.GetCollection<T>(Table);
             });
         }
 
         public void InitIndex()
         {
-            LitedbCollection.EnsureIndex(x => x.PID);
-            //LiteDBHelper.Open(db =>
-            //{
-            //    var plans = db.GetCollection<T>(Table);
-            //    plans.EnsureIndex(x => x.PID);
-            //});
+            DataList.EnsureIndex(x => x.PID);
         }
 
         public void NewPlan(T t)
         {
-            LitedbCollection.Insert(t);
-            //LiteDBHelper.Open(db =>
-            //{
-            //    var plans = db.GetCollection<T>(Table);
-            //    plans.Insert(t);
-            //});
+            DataList.Insert(t);
         }
 
         public void ModifyPlan(T t)
         {
-            LitedbCollection.Update(t);
-            //LiteDBHelper.Open(db =>
-            //{
-            //    var plans = db.GetCollection<T>(Table);
-            //    plans.Update(t);
-            //});
+            DataList.Update(t);
         }
 
         public void DeletePlan(string id)
         {
-            LitedbCollection.Delete(id);
-            //LiteDBHelper.Open(db =>
-            //{
-            //    var plans = db.GetCollection<Plan>(Table);
-            //    plans.Delete(id);
-            //});
+            DataList.Delete(id);
+           
         }
 
         public List<T> Find(Func<LiteCollection<T>, List<T>> listAction)
         {
             List<T> list = new List<T>();
-            list = listAction(LitedbCollection);
-
-            //LiteDBHelper.Open(db =>
-            //{
-            //    var dbList = db.GetCollection<T>(Table);
-
-            //    list = listAction(dbList);
-            //});
+            list = listAction(DataList);
 
             return list;
         }
--- DailyHome.App/Services/PlanService.cs
+++ DailyHome.App/Services/PlanService.cs
@@ -10,13 +10,27 @@ namespace DailyHome.App.Services
 {
     public class PlanService:LiteDBService<Plan>
     {
+        public PlanService():base()
+        {
+
+        }
+
+        public PlanService(string connString):base(connString)
+        {
+
+        }
+
+        private void Test()
+        {
+            
+        }
+
         public List<DateTime> GenScheduler(string crontab,DateTime start,DateTime end)
         {
             var s = CrontabSchedule.Parse(crontab);
             var occurrences = s.GetNextOccurrences(start, end);
             
             return occurrences.ToList();
-
         }
 
          
